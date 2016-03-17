6fa96bede415f27b387e2c6684f2bc44db60cc4e
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2016-03-17 07:50:18 +0100

    update openwrt module

--- modules/openwrt.sh
+++ modules/openwrt.sh
@@ -6,32 +6,15 @@ if [ -f /etc/openwrt_release ]; then
     '/openwrt_help')
       msg=$'Openwrt module commands:\n/{wifi,dhcp}'
     ;;
+    'c'|'clients')
+        source $pdir/$modules_dir/openwrt/show_wifi_clients.sh
+      ;;
     '/wifi')
       case $args in
       h|help)
-        msg=$'Allowed parameters:\nno param toggles wifi\n/wifi {,0,1,on,off,s,status,c,clients}'
+        msg=$'Allowed parameters:\n/wifi {,0,1,on,off,t,toggle,c,clients}'
       ;;
-      s|status)
-        if [ $(uci get wireless.@wifi-iface[$wifi_device].disabled) -eq 0 ]; then
-          msg="wifi is on"
-        else
-          msg="wifi is off"
-        fi
-      ;;
-      c|clients)
-        source $pdir/$modules_dir/openwrt/show_wifi_clients.sh
-      ;;
-      0|off)
-        wifi down
-        uci set wireless.@wifi-iface[$wifi_device].disabled=1
-        msg="wifi disabled"
-      ;;
-      1|on)
-        wifi up
-        uci set wireless.@wifi-iface[$wifi_device].disabled=1
-        msg="wifi disabled"
-      ;;
-      *)
+      t|toggle)
         case $(uci get wireless.@wifi-iface[$wifi_device].disabled) in
           0)
             wifi down
@@ -44,6 +27,26 @@ if [ -f /etc/openwrt_release ]; then
             msg="wifi enabled"
           ;;
         esac
+        uci commit wireless
+      ;;
+      0|off)
+        wifi down
+        uci set wireless.@wifi-iface[$wifi_device].disabled=1
+        uci commit wireless
+        msg="wifi disabled"
+      ;;
+      1|on)
+        wifi up
+        uci set wireless.@wifi-iface[$wifi_device].disabled=1
+        uci commit wireless
+        msg="wifi disabled"
+      ;;
+      *)
+        if [ $(uci get wireless.@wifi-iface[$wifi_device].disabled) -eq 0 ]; then
+          msg="wifi is on"
+        else
+          msg="wifi is off"
+        fi
       esac
     ;;
     '/dhcp')
