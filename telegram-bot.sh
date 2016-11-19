cb66cb483faaea98aa9aa8c94b534b1f8cae8eca
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2016-11-19 17:17:24 +0100

    script updates

--- telegram-bot.sh
+++ telegram-bot.sh
@@ -1,169 +1,266 @@
 #!/bin/sh
-# Telegram bot written in shell scripting
-# Original authors by @topkecleon, Juan Potato (@awkward_potato) and RG72
-# http://github.com/topkecleon/bashbot
-# http://github.com/RG72/telegram-bot-bash
-# http://github.com/viralex/telegram-bot.sh
-# for ash you need to enable getopt option and -l option in busybox
+# Telegram bot written in shell scripting.
+# Forked from RG72/telegram-bot-bash , topkecleon/telegram-bot-bash
 
-PNAME=`basename $0`
-VERSION="0.1"
+SCRIPT_NAME=$(basename $0)
+VERSION="0.2"
 
-#### init
-pdir=$(dirname $0)
-[ -f $pdir/config.sh ] && source $pdir/config.sh || (echo "please configure: copy config.sh.orig => config.sh and set token." && exit 1)
-[ -f $pdir/functions.sh ] && source $pdir/functions.sh || (echo "err... sorry I must go!" && exit 1)
+TOKEN=""
+OWNER=""
+OWNER_CHATID=""
+DEVICE="$HOSTNAME"
 
-### functions
+SCRIPT_DIR=$(dirname $0)
+CONFIG_DIR="$HOME/.config/tbot"
+MODULE_DIR="$SCRIPT_DIR/modules"
+CHAT_DIR="$CONFIG_DIR/chats"
 
-opt_version()
+CONFIG_FILE="$CONFIG_DIR/config"
+LOCK_FILE="/tmp/lock"
+
+NOTIFY_LOGIN=yes
+NOTIFY_OWNER=yes
+REPLY_ANYONE=yes
+ENABLE_COMMANDS=yes
+ENABLE_SLEEP=yes
+
+CYCLE_SLEEP_TIME=2
+STANDBY_SLEEP_TIME=1
+STANDBY_TIME=30
+TIMEOUT=10
+
+#### text output functions #####################################################
+
+QUIET_FLAG=no
+DEBUG_FLAG=no
+
+echoc ()
 {
-  echo -e "$PNAME-$VERSION"
-  exit 0
+  [ $QUIET_FLAG = no ] && echo -e "\E["$1"m$2\E[0m"
 }
 
-opt_help()
+debug () 
 {
-  echo -e "$PNAME-$VERSION"
-  echo -e "\nhelp:\n"
-  exit 0
+  [ $DEBUG_FLAG = yes ] && echoc 31 "$*"
 }
 
-run_daemon()
+info ()
 {
-  echo "bot_dir: "$(pwd)
+  echoc 30 "$*"
+}
 
-  get_name
-  bot_username=$res
-  echo "bot_username: $bot_username"
-  #[ $enable_notify_login -eq 1 ] && send_message_all "$bot_username started"
+error ()
+{
+  echoc 32 "$*"
+}
+
+#### telegram I/O functions ####################################################
+
+JSON="$SCRIPT_DIR/JSON.sh"
+URL_TELEGRAM='https://api.telegram.org/bot'
+HTTPS_EXEC="wget -qO- --no-check-certificate -T $TIMEOUT"
+
+telegram_exec()
+{
+  $HTTPS_EXEC --post-data="$2" ${URL_TELEGRAM}$TOKEN/$1
+}
+
+send_message()
+{
+  telegram_exec "sendMessage" "chat_id=$1&text=$2" &>/dev/null
+}
+
+send_message_all()
+{
+  CHATS=`ls $CHAT_DIR"/"` &>/dev/null
+  for user in $CHATS; do
+    source "$CHAT_DIR/$user"
+    send_message "$CHATID" "$1"
+  done
+}
+
+get_message()
+{
+  ret=$(telegram_exec "getUpdates?offset=$1&limit=1") &>/dev/null
+
+  if [ ! "$ret" == '{"ok":true,"result":[]}' ]; then
+    CHATID=$(echo $ret | $JSON | egrep '\["result",0,"message","chat","id"\]' | cut -f 2)
+    FROM=$(echo $ret | $JSON | egrep '\["result",0,"message","from","username"\]' | cut -f 2 | cut -d '"' -f 2)
+    OFFSET=$(echo $ret | $JSON | egrep '\["result",0,"update_id"\]' | cut -f 2)
+    MESSAGE=$(echo $ret | $JSON -s | egrep '\["result",0,"message","text"\]' | cut -f 2 | cut -d '"' -f 2)
+    MESSAGE_ID=$(echo $ret | $JSON | egrep '\["result",0,"message","message_id"\]' | cut -f 2 )
+    return 0
+  fi
+  return 1
+}
+
+get_bot_username()
+{
+  ret=$(telegram_exec getMe | $JSON -s | egrep '\["result","username"\]' | cut -f 2 | cut -d '"' -f 2) &>/dev/null
+  BOTNAME=$ret
+  debug "bot username: $BOTNAME"
+}
+
+send_markdown_message()
+{
+  telegram_exec "$URL_TELEGRAM/sendMessage" "chat_id=$1&text=$2&parse_mode=markdown" &>/dev/null
+}
+
+send_doc()
+{
+  telegram_exec "sendDocument" "chat_id=$1&document=@$2" &>/dev/null
+}
+
+send_photo()
+{
+  telegram_exec "sendPhoto" "chat_id=$1&photo=@$2" &>/dev/null
+}
+
+#### option actions ############################################################
+
+opt_run_daemon()
+{
+  get_bot_username
+
+  [ $NOTIFY_LOGIN ] && send_message_all "$BOTNAME started"
 
   OFFSET=0
   PREV_TIME=0
   while true; do
-  {
     while true; do
       get_message $OFFSET
-      [ $? == 0 ] && break;
+      [ $? -eq 0 ] && break;
     done
 
-    echo "recv: $FROM $MESSAGE"
+    [ -z "$FROM" ] && continue || echo "[$FROM] >> $MESSAGE"
 
-    CURR_TIME=`date +%s` #CURR_TIME=$((10#`date +%s`))
+    CURR_TIME=$(date +%s) #CURR_TIME=$((10#`date +%s`))
     OFFSET=$((OFFSET+1))
 
-    if [ ! "$FROM" == "\"$OWNER\"" ]; then
-      echo "you are not the owner!"
-      continue
+    if [ "$FROM" != "$OWNER" ]; then
+      [ $NOTIFY_OWNER = yes ] && send_message "$OWNER_CHATID" "user is talking to me: \"$FROM\" => \"$MESSAGE\""
+      # TODO reply only to selected users
+      [ ! $REPLY_ANYONE = no ] && continue || \
+      echo "CHATID=\"$CHATID\"" >> "$CHAT_DIR/$FROM"
     fi
 
     if [ $OFFSET != 1 ]; then
-      echo "$OFFSET" > $last_offset_file
       # sorry, busybox's ash does not support arrays
-      cmd=`echo $MESSAGE | awk '{print $1}'`
-      args=`echo $MESSAGE | awk '{$1=""; print $0}' | cut -c2-`
-      echo "cmd: \"$cmd\" args: \"$args\""
-      command_found=no
-      if [ $enable_commands -eq 1 ]; then
-        for f in $pdir/$modules_dir/* ; do
-            #f="$pdir/$modules_dir/"$(basename $f)
-            [ -d $f ] && continue
-            if grep -q "'$cmd')" "$f"; then  # or $cmd| or |$cmd
-              echo "command found at: \"$f\"" #disable blocks of commands using exec bit
-              command_found=yes
-              [ -x $f ] && source $f || msg="command disabled"
+      COMMAND=$(echo $MESSAGE | awk '{print $1}')
+      ARGUMENTS=$(echo $MESSAGE | awk '{$1=""; print $0}' | cut -c2-)
+      found=no
+      if [ $ENABLE_COMMANDS = yes ]; then
+        for module in $MODULE_DIR/* ; do
+            [ -d $module ] && continue
+            if grep -q "'$COMMAND')" "$module"; then
+              found=yes
+              [ -x $module ] && source $module || msg="\"$module\" is disabled"
               break
             fi
         done
-
-        if [ $command_found == no ]; then
-          echo "command not found"
-          msg="command not found"
-        fi
+        [ $found == no ] && (msg="command not found" && echo $msg)
       fi
 
-      if [ ! -z "$msg" ]; then
-        PREV_TIME=$CURR_TIME
-        send_message "$TARGET" "$msg" &>/dev/null
+      if [ -n "$msg" ]; then
+        send_message "$CHATID" "$msg"
       fi
+      PREV_TIME=$CURR_TIME
     fi
 
     #elapsed=$((CURR_TIME-PREV_TIME))
-    #if [ $elapsed -le $standby ]; then
-    #  if [ $cycle_sleep -gt 0 ]; then
-        sleep $cycle_sleep
+    #if [ $elapsed -le $STANDBY_TIME ]; then
+    #  if [ $CYCLE_SLEEP_TIME -gt 0 ]; then
+    sleep $CYCLE_SLEEP_TIME
     #  fi
     #else
-    #  sleep $standby_sleep
+    #  sleep $STANDBY_SLEEP_TIME
     #fi
-  }
   done
 }
 
 opt_message()
 {
-  s=0
-  chats=`ls $chat_dir"/"` &>/dev/null
-  for chat in $chats; do
-    stream=`cat "$chat_dir/$chat"`
-    stream=$((10#$stream))
-
-    if [ $s -eq $stream ]; then
-      echo -e "---\nchat: $chat\nfile: \"$file\"\ntext: \"$text\"\n---\n"
-
-      if [ $markdown_flag == no ]; then
-        send_message "$chat" "$text" &>/dev/null
-      else
-        send_markdown_message "$chat" "$text" &>/dev/null
-      fi
-
-      if [ ! -z "$file" ]; then
-        send_doc "$chat" "$file" &>/dev/null
-      fi
+  if [ $ALL_FLAG ]; then
+    send_message_all "$TEXT"
+  else
+    if [ -z $DEST_USER ]; then
+      CHATID=$OWNER_CHATID
+    else
+      source "$CHAT_DIR/$DEST_USER"
     fi
-  done
+    
+    if [ -n "$TEXT" ]; then
+      if [ $MARKDOWN_FLAG == no ]; then
+        send_message "$CHATID" "$TEXT"
+      else
+        send_markdown_message "$CHATID" "$TEXT"
+      fi
+    elif [ -n "$FILE" ]; then
+      send_doc "$CHATID" "$FILE"
+    elif [ -n "$IMG" ]; then
+      send_photo "$CHATID" "$IMG"
+    fi
+  fi
 }
 
-#### process parameters
+opt_version()
+{
+  echo "$SCRIPT_NAME-$VERSION"
+  exit 0
+}
 
-quiet_flag=no
-daemon_flag=yes
-markdown_flag=no
+opt_help()
+{
+  echo "$SCRIPT_NAME-$VERSION"
+  echo -e "\nhelp: wip...\n"
+  exit 0
+}
 
-if type "getopt" &> /dev/null; then
-  if ! options=$(getopt -o hvqDmt:f: -l \
-               help,version,quiet,daemon,markdown,text:,file:  -- "$@")
-  then
-      exit 1
-  fi
+#### process parameters ########################################################
 
-  set -- $options
+DAEMON_FLAG=no
+if ! type "getopt" &> /dev/null; then
+  echo -e "no getopt support, running in as daemon..."
+  DAEMON_FLAG=yes
+else
+  ALL_FLAG=no
+  MARKDOWN_FLAG=no
+  DEST_USER=""
 
-  while [ $# -gt 0 ]
-  do
+  OPTIONS=$(getopt -o hvdqDmat:f:i:u: -l help,version,debug,quiet,daemon,markdown,all,text:,file:,img:,user: -- "$@")
+  [ $? ] || exit 1
+  set -- $OPTIONS
+
+  while [ $# -gt 0 ]; do
     case $1 in
-      -t|--text) shift; daemon_flag=no; text="$1";;
-      -f|--file) shift; file="$*";;
-      -h|--help) opt_help ;;
-      -v|--version) opt_version ;;
-      -q|--quiet) quiet_flag=yes;;
-      -D|--daemon) daemon_flag=yes;;
-      -m|--markdown) markdown_flag=yes;;
+      -v|--version) opt_version;;
+      -h|--help) opt_help;;
+      -q|--quiet) QUIET_FLAG=yes;;    
+      -d|--debug) DEBUG_FLAG=yes;;
+      -t|--text) shift; TEXT="$1";;
+      -a|--all) ALL_FLAG=yes;;
+      -f|--file) shift; FILE="$*";;
+      -i|--img) shift; IMG="$*";;
+      -u|--user) shift; DEST_USER="$1";;
+      -D|--daemon) DAEMON_FLAG=yes;;
+      -m|--markdown) MARKDOWN_FLAG=yes;;
       (--) shift; break;;
-      (-*) echo "$PNAME: error - unrecognized option $1" 1>&2; exit 1;;
+      (-*) echo "unrecognized option $1" 1>&2; exit 1;;
       (*) break;;
     esac
     shift
   done
-
-  #### execute actions
-
-  if [ $daemon_flag == no ]; then
-    opt_message
-    exit 0
-  fi
-else
-  echo -e "please, add getopt support.\nrunning in as daemon..."
+  [ -n "$TEXT" ] && DAEMON_FLAG=no
+  [ -n "$FILE" ] && DAEMON_FLAG=no
+  [ -n "$IMG"  ] && DAEMON_FLAG=no
 fi
 
-run_daemon
+if [ -f ${CONFIG_FILE} ]; then
+  source ${CONFIG_FILE}
+  debug "Loading ${CONFIG_FILE} ..."
+else
+  debug "Please, configure \"${CONFIG_FILE}\" setting TOKEN,OWNER"
+  exit 1
+fi
+
+[ $DAEMON_FLAG = yes ] && opt_run_daemon || (opt_message && exit 0)
